87598563ec5595fc4c726ebc3b4929033474e34c
IONHAX04 <thirukumara04@gmail.com>
2024-04-20 23:24:27 +0530

    first commit

---
+++ app/src/main/java/com/ionhax/animations/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.ionhax.animations
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.ScrollView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private lateinit var scrollView: ScrollView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        imageView = findViewById(R.id.addId);
+        scrollView = findViewById(R.id.scrollView)
+        imageView.setOnClickListener {
+            scrollView.visibility = View.VISIBLE
+        }
+
+    }
+}
